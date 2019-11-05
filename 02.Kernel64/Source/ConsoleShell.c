50b7db6b4257b1a9ecd6372b5f2b5c7680ba9510
HJT <pibii9265@outlook.com>
2019-11-05 13:39:19 +0900

    modify raisefault

--- 02.Kernel64/Source/ConsoleShell.c
+++ 02.Kernel64/Source/ConsoleShell.c
@@ -2,6 +2,9 @@
 #include "Console.h"
 #include "Keyboard.h"
 #include "Utility.h"
+#include "../../01.Kernel32/Source/Page.h"
+
+int bTry = 0;
 
 // 커맨드 테이블 정의
 SHELLCOMMANDENTRY gs_vstCommandTable[] =
@@ -413,10 +416,29 @@ void kShutdown( const char* pcParamegerBuffer )
     kReboot();
 }
 
+static inline void invlpg(void* m)
+{
+    asm volatile ( "invlpg (%0)" : : "b"(m) : "memory" );
+}
+
 void kRaiseFault( const char* pcParamegerBuffer )
 {
+    PTENTRY* pstPTEntry = ( PTENTRY* ) 0x142000;
+    PTENTRY* pstEntry = &(pstPTEntry[511]);
     DWORD* pstPage = 0x1ff000;
-    *pstPage = 1;
+    kPrintf("%d", bTry);
+    if(bTry == 0){
+        pstEntry->dwAttributeAndLowerBaseAddress = 0x2;
+        invlpg(0x1ff000);
+        *pstPage = 1;
+        bTry++;
+    }
+    else if (bTry == 1) {
+        pstEntry->dwAttributeAndLowerBaseAddress = 0x1;
+        invlpg(0x1ff000);
+        *pstPage = 1;
+        bTry++;
+    }
 }
 
 void kHistoryPrint(char* vcCommandBuffer, char historyBuffer[][CONSOLESHELL_MAXCOMMANDBUFFERCOUNT],
